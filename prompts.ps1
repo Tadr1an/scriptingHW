d2c2dc09bfa9edcbfdaafb10c1c75e38726e65c4
Tadrian Davis <121205610+Tadr1an@users.noreply.github.com>
2023-11-10 14:31:43 -0600

    Update prompts.ps1

--- prompts.ps1
+++ prompts.ps1
@@ -3,15 +3,30 @@
 #unzips Requirements1
 Expand-Archive "C:\Users\LabAdmin\Desktop\Requirements1.zip" "C:\Users\LabAdmin\Desktop\Requirements1"
 
-#declares user input variable
-$userInput = 0
-
-while($userInput -ne 5) {
+try {
+    
+}
+catch {
+    #alerts user if they're out of memory
+    [System.OutOfMemoryException]
+    Write-Host "Out of memory. Please close some programs to continue."; 
+}
+#continue script
+finally {
+#while loop to cycle through user's input options
+while(1 -ne 100) {
     switch (Read-Host "(1. to list files) (2. list files in tabular) (3. list CPU and RAM usage) (4. list processes) (5. exit)")
     {
+        #lists folder contents and appends to DailyLog.txt
         1 {Get-Date; Get-ChildItem C:\Users\LabAdmin\Desktop\Requirements1\*.log >> C:\Users\LabAdmin\Desktop\Requirements1\DailyLog.txt}
-        2 {Get-ChildItem C:\Users\LabAdmin\Desktop\Requirements1 | Sort-Object Name -Descending | Format-Table > C:\Users\LabAdmin\Desktop\Requirements1\C916contents.txt}
+        #lists folder contents in tabular format and appends to C916contents.txt
+        2 {Get-ChildItem C:\Users\LabAdmin\Desktop\Requirements1 | Sort-Object Name -Descending | Format-Table > C:\Users\LabAdmin\Desktop\Requirements1\C916contents.txt; echo "file created"}
+        #lists CPU and RAM usage
         3 {Get-Counter '\Memory\Available MBytes'; Get-Counter '\Processor(_Total)\% Processor Time'}
+        #lists processes and sorts as grid
         4 {Get-Process | Sort-Object WS | Format-Table }
-        5 {"You have exited."; break}
+        #exits script
+        5 {"You have exited."; exit}
     }
+}
+}
