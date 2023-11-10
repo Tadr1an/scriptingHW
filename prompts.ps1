fab17fe2824a2e0e28aaf67c3152c7177a551518
Tadrian Davis <121205610+Tadr1an@users.noreply.github.com>
2023-11-10 02:14:41 -0600

    Update prompts.ps1

--- prompts.ps1
+++ prompts.ps1
@@ -1,14 +1,17 @@
 # Tadrian Davis 011332686
+
+#unzips Requirements1
 Expand-Archive "C:\Users\LabAdmin\Desktop\Requirements1.zip" "C:\Users\LabAdmin\Desktop\Requirements1"
 
+#declares user input variable
 $userInput = 0
 
 while($userInput -ne 5) {
-    switch (Read-Host "1. to list files" `n "2. list files in tabular" `n "3. list CPU and RAM usage" `n "4. list processes" `n "5. exit") 
-        1 {Get-Date
-           Get-ChildItem C:\Users\LabAdmin\Desktop\Requirements1\*.log >> C:\Users\LabAdmin\Desktop\Requirements1\DailyLog.txt}
+    switch (Read-Host "(1. to list files) (2. list files in tabular) (3. list CPU and RAM usage) (4. list processes) (5. exit)")
+    {
+        1 {Get-Date; Get-ChildItem C:\Users\LabAdmin\Desktop\Requirements1\*.log >> C:\Users\LabAdmin\Desktop\Requirements1\DailyLog.txt}
         2 {Get-ChildItem C:\Users\LabAdmin\Desktop\Requirements1 | Sort-Object Name -Descending | Format-Table > C:\Users\LabAdmin\Desktop\Requirements1\C916contents.txt}
         3 {Get-Counter '\Memory\Available MBytes'; Get-Counter '\Processor(_Total)\% Processor Time'}
         4 {Get-Process | Sort-Object WS | Format-Table }
-        5 {break}
+        5 {"You have exited."; break}
     }
