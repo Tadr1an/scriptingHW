6188cab5dd6a1c054a56d07456315aa7d73b81e3
Tadrian Davis <121205610+Tadr1an@users.noreply.github.com>
2023-11-10 01:23:55 -0600

    Update prompts.ps1

--- prompts.ps1
+++ prompts.ps1
@@ -1,13 +1,14 @@
 # Tadrian Davis 011332686
 Expand-Archive "C:\Users\LabAdmin\Desktop\Requirements1.zip" "C:\Users\LabAdmin\Desktop\Requirements1"
 
-New-Item C:\Users\LabAdmin\Desktop\Requirements1\DailyLog.txt
+$userInput = 0
 
-switch (Read-Host "Select a number 1-4. Enter 5 to quit.") {
-    1 { date
-    ls C:\Users\LabAdmin\Desktop\Requirements1\*.log >> C:\Users\LabAdmin\Desktop\Requirements1\DailyLog.txt}
-    2
-    3
-    4
-    5
-}
+while($userInput -ne 5) {
+    switch (Read-Host "1. to list files" `n "2. list files in tabular" `n "3. list CPU and RAM usage" `n "4. list processes" `n "5. exit") 
+        1 {Get-Date
+           Get-ChildItem C:\Users\LabAdmin\Desktop\Requirements1\*.log >> C:\Users\LabAdmin\Desktop\Requirements1\DailyLog.txt}
+        2 {Get-ChildItem C:\Users\LabAdmin\Desktop\Requirements1 | Sort-Object Name -Descending | Format-Table > C:\Users\LabAdmin\Desktop\Requirements1\C916contents.txt}
+        3 {Get-Counter '\Memory\Available MBytes'; Get-Counter '\Processor(_Total)\% Processor Time'}
+        4 {Get-Process | Sort-Object WS | Format-Table }
+        5 {break}
+    }
