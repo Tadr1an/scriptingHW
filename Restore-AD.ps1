e53fc80f4a5488bdaf6f3c3a776e314a966af3de
Tadrian Davis <121205610+Tadr1an@users.noreply.github.com>
2023-11-11 15:04:40 -0800

    Update Restore-AD.ps1

--- Restore-AD.ps1
+++ Restore-AD.ps1
@@ -1,28 +1,15 @@
-# Tadrian Davis 011332686
-
-$finance = Get-ADOrganizationalUnit -Filter {Name -eq "Finance"}
-
-if ($finance -ne $null) {
-    Write-Output "Finance exists. It will be deleted now."; Remove-ADOrganizationalUnit -Identity "OU=Finance"
-} else {
-    Write-Output "Finance does not exist."
-}
-
-New-ADOrganizationalUnit -Name "Finance" -Path "DC=consultingfirm,DC=COM" -ProtectedFromAccidentalDeletion $False
-
-$NewADUsers = Import-Csv -Path "C:\Users\LabAdmin\Desktop\Requirements2\financePersonnel.csv"
-
 $path = "OU=Finance,DC=consultingfirm,DC=COM"
 foreach ($newUser in $NewADUsers)
 {
-    $firstName = $newUser.FirstName
-    $lastName = $newUser.LastName
+    $firstName = $newUser.FirstName -replace '\W','' # Remove non-alphanumeric characters
+    $lastName = $newUser.LastName -replace '\W','' # Remove non-alphanumeric characters
     $displayName = $firstName + " " + $lastName
+    $userName = ($firstName + $lastName).Trim() # Create username and remove any leading/trailing spaces
     $postalCode = $newUser.PostalCode
     $officePhone = $newUser.OfficePhone
     $mobilePhone = $newUser.MobilePhone
 
-    New-ADUser  -Name $displayName `
+    New-ADUser  -Name $userName ` # Use the username as the Name
                 -GivenName $firstName `
                 -Surname $lastName `
                 -DisplayName $displayName `
