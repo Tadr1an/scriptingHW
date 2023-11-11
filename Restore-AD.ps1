c8a1d77cca24be5ca6ae2ace9aec3304c87560f6
Tadrian Davis <121205610+Tadr1an@users.noreply.github.com>
2023-11-11 13:55:11 -0800

    Create Restore-AD.ps1

---
+++ Restore-AD.ps1
@@ -0,0 +1,36 @@
+# Tadrian Davis 011332686
+
+$finance = Get-ADOrganizationalUnit -Filter {Name -eq "Finance"}
+
+if ($finance -ne $null) {
+    Write-Output "Finance exists. It will be deleted now."; Remove-ADOrganizationalUnit -Identity "OU=Finance"
+} else {
+    Write-Output "Finance does not exist."
+}
+
+New-ADOrganizationalUnit -Name "Finance" -Path "DC=consultingfirm,DC=COM" -ProtectedFromAccidentalDeletion $False
+
+$NewADUsers = Import-Csv -Path "C:\Users\LabAdmin\Desktop\Requirements2\financePersonnel.csv"
+
+$path = "OU=Finance,DC=consultingfirm,DC=COM"
+foreach ($newUser in $NewADUsers)
+{
+    $firstName = $newUser.FirstName
+    $lastName = $newUser.LastName
+    $displayName = $firstName + " " + $lastName
+    $postalCode = $newUser.PostalCode
+    $officePhone = $newUser.OfficePhone
+    $mobilePhone = $newUser.MobilePhone
+
+    New-ADUser  -Name $displayName `
+                -GivenName $firstName `
+                -Surname $lastName `
+                -DisplayName $displayName `
+                -PostalCode $postalCode `
+                -OfficePhone $officePhone `
+                -MobilePhone $mobilePhone `
+                -Path $path `
+
+}
+
+Get-ADUser -Filter * -SearchBase “ou=Finance,dc=consultingfirm,dc=com” -Properties DisplayName,PostalCode,OfficePhone,MobilePhone > .\AdResults.txt
