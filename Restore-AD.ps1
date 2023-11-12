38d08a0f6c42ebbf8eac09056a0150a637a2372c
Tadrian Davis <121205610+Tadr1an@users.noreply.github.com>
2023-11-12 13:15:24 -0600

    Restore-AD.ps1
    restore ad final version

--- Restore-AD.ps1
+++ Restore-AD.ps1
@@ -1,18 +1,26 @@
 # Tadrian Davis 011332686
 
-$finance = Get-ADOrganizationalUnit -Filter {Name -eq "Finance"}
+#creates variable for the Finance OU
+$finance = Get-ADOrganizationalUnit -Filter {Name -eq "Finance"} #creates variable for the Finance OU
 
+
+#deletes the Finance OU if it exists
 if ($finance -ne $null) {
     Write-Output "Finance exists. It will be deleted now."; Remove-ADOrganizationalUnit -Identity "OU=Finance"
 } else {
     Write-Output "Finance does not exist."
 }
 
+#creates the Finance OU
 New-ADOrganizationalUnit -Name "Finance" -Path "DC=consultingfirm,DC=COM" -ProtectedFromAccidentalDeletion $False
+Write-Output "Finance OU has been created."
 
+#imports the csv file containing the finance users
 $NewADUsers = Import-Csv -Path "C:\Users\LabAdmin\Desktop\Requirements2\financePersonnel.csv"
 
+#defines the path for the Finance OU
 $path = "OU=Finance,DC=consultingfirm,DC=COM"
+#assigns the variables to the csv file columns
 foreach ($newUser in $NewADUsers)
 {
     $firstName = $newUser.First_Name
@@ -21,7 +29,7 @@ foreach ($newUser in $NewADUsers)
     $postalCode = $newUser.PostalCode
     $officePhone = $newUser.OfficePhone
     $mobilePhone = $newUser.MobilePhone
-
+#creates the new users in the Finance OU
     New-ADUser  -Name $displayName `
                 -GivenName $firstName `
                 -Surname $lastName `
@@ -33,4 +41,5 @@ foreach ($newUser in $NewADUsers)
 
 }
 
+#creates the AdResults.txt file
 Get-ADUser -Filter * -SearchBase “ou=Finance,dc=consultingfirm,dc=com” -Properties DisplayName,PostalCode,OfficePhone,MobilePhone > .\AdResults.txt
