5268058d44fc83833400faa92ca5b9a07c1a7c17
Tadrian Davis <121205610+Tadr1an@users.noreply.github.com>
2023-11-17 13:42:18 -0800

    Add files via upload

---
+++ createTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Client_A_Contacts (
+     first_name varchar(100),
+     last_name varchar(100),
+     city varchar(50),
+     county varchar(50)
+     zip varchar(50)
+     officePhone varchar(50)
+     mobilePhone varchar(50)
+);
