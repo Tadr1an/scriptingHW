825f18ca51666338430a555e405c7a7cd29fca01
Tadrian Davis <121205610+Tadr1an@users.noreply.github.com>
2023-11-19 15:50:55 -0600

    update prompts ps1 for 3rd submission
    added lines to create dailylog.txt and c916contents.txt

--- prompts.ps1
+++ prompts.ps1
@@ -11,11 +11,16 @@ catch {
 #continue script
 finally {
 #while loop to cycle through user's input options
+
+#create C:\Users\LabAdmin\Downloads\Requirements1\DailyLog.txt and C:\Users\LabAdmin\Downloads\Requirements1\C916contents.txt
+New-Item -Path C:\Users\LabAdmin\Downloads\Requirements1\DailyLog.txt -ItemType File -Force
+New-Item -Path C:\Users\LabAdmin\Downloads\Requirements1\C916contents.txt -ItemType File -Force
+
 while(1 -ne 100) {
     switch (Read-Host "(1. to list files) (2. list files in tabular) (3. list CPU and RAM usage) (4. list processes) (5. exit)")
     {
         #lists folder contents and appends to DailyLog.txt
-        1 {Get-Date; Get-ChildItem C:\Users\LabAdmin\Downloads\Requirements1\*.log >> C:\Users\LabAdmin\Downloads\Requirements1\DailyLog.txt}
+        1 {Get-Date >> C:\Users\LabAdmin\Downloads\Requirements1\DailyLog.txt; Get-ChildItem C:\Users\LabAdmin\Downloads\Requirements1\*.log >> C:\Users\LabAdmin\Downloads\Requirements1\DailyLog.txt; echo "file created"}
         #lists folder contents in tabular format and appends to C916contents.txt
         2 {Get-ChildItem C:\Users\LabAdmin\Downloads\Requirements1 | Sort-Object Name -Descending | Format-Table > C:\Users\LabAdmin\Downloads\Requirements1\C916contents.txt; echo "file created"}
         #lists CPU and RAM usage
